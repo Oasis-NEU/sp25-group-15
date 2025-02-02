feb077f5f7a6f281e4fd591520946afa1388276b
Elaine <elainemo5510@gmail.com>
2025-02-02 14:02:11 -0500

    started dropdown, many folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+
+  /*return(
+    <div>
+      <p>World</p>
+    </div>
+  )*/
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>Protein Pow</h1>
+      <div className="filter price">
+        <div className = "content">
+          <Dropdown
+          buttonText="Dropdown button" content = {<p>Hello World!</p>} />
+        </div>
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -30,6 +32,7 @@ function App() {
       </p>
     </>
   )
+
 }
 
 export default App
