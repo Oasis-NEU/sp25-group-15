626a2f2677bcb0ddff982e6e393c8e4dde11994a
mikethebike222 <michaelantlucio@gmail.com>
2025-02-02 14:00:46 -0500

    backend folder, instructions for api

---
+++ Backend/main.py
@@ -0,0 +1,12 @@
+# import amazon api key(dont have key yet associates loading 1-2 day wait)
+# import flask(python library)
+
+# Fetch from React get the information whether it be chocolate protien powder, creatine or whatever it is
+
+# Send data to amazon api and get back the returning giga string 
+
+# Parse the data, could use openai for this.
+
+# Send info to frontend
+# price per serving, links, images, reviews ouit of 5 or anything else  
+
